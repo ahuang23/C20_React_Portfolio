b369a03bf8b0a430b90357972619b0673d696bf2
Alan Huang <a.huang232@gmail.com>
2022-11-26 13:57:41 -0800

    Set baseline

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Navigation from './Navigation';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <h1>Test</h1>
+      <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+
+      {renderPage()}
+    </div>
+  );
+}
---
+++ src/components/pages/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div className="title">
+      <h1>Home Page</h1>
+      <p>
+            Hello, my name is Alan Huang
+      </p>
+    </div>
+  );
+}
